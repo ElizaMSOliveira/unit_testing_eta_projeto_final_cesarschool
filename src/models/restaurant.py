9e840cd22268640f87ec7d47bb9a6ef82e549ab5
ElizaMSOliveira <elizamsoliveira@gmail.com>
2023-09-04 21:38:06 -0300

    Tests da classe test_restaurant.py desenvolvido

--- src/models/restaurant.py
+++ src/models/restaurant.py
@@ -8,40 +8,40 @@ class Restaurant:
         self.open = False
 
     def describe_restaurant(self):
-        """Imprima uma descrição simples da instância do restaurante."""  # retur no lugar de print e ajustes de retorno
-        # retunr no lugar de print
+        """Imprima uma descrição simples da instância do restaurante."""
+        # return no lugar de print
         return f"Esse restaturante chama: {self.restaurant_name} and serve: {self.cuisine_type}, está servindo: {self.number_served} consumidores desde que está aberto."
 
     def open_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está aberto para negócios."""
         if not self.open:
             self.open = True  # Recebendo True para ser aberto
-            self.number_served = 2  # Adicionando valor positivo a quantidade de cliantes ja que acabamos de abrir, apesar que esse não faz diferença pro metodo
-            return f"{self.restaurant_name} agora está aberto!"# retunr no lugar de print
+            # self.number_served = -2 # Não precisa atribuir valor para funcionamento do metodo
+            return f"{self.restaurant_name} agora está aberto!"  # return no lugar de print
         else:
-            return f"{self.restaurant_name} já está aberto!"# retunr no lugar de print
+            return f"{self.restaurant_name} já está aberto!"  # return no lugar de print
 
     def close_restaurant(self):
         """Imprima uma mensagem indicando que o restaurante está fechado para negócios."""
         if not self.open:
-            #self.number_served = 0 # Se ta fechado não faz sentido atribuir valor
-            return f"{self.restaurant_name} já está fechado!"# retunr no lugar de print
+            # self.number_served = 0 # Se ta fechado não faz sentido atribuir valor
+            return f"{self.restaurant_name} já está fechado!"  # retunr no lugar de print
         else:
-            self.open = False # Fechando o restaurante já que o metodo para ser testado recebeu ele aberto(True)
-            return f"{self.restaurant_name} agora está fechado!"# retunr no lugar de print
+            self.open = False  # Fechando o restaurante já que o metodo para ser testado recebeu ele aberto(True)
+            return f"{self.restaurant_name} agora está fechado!"  # retunr no lugar de print
 
     def set_number_served(self, total_customers):
         """Defina o número total de pessoas atendidas por este restaurante até o momento."""
         if self.open:
             self.number_served = total_customers
-            return self.number_served # Adicionando o retorno
+            return self.number_served  # Adicionando o return
         else:
-            return f"{self.restaurant_name} está fechado!"# retunr no lugar de print
+            return f"{self.restaurant_name} está fechado!"  # retunr no lugar de print
 
     def increment_number_served(self, more_customers):
         """Aumenta número total de clientes atendidos por este restaurante."""
         if self.open:
-            self.number_served += more_customers # Incrementando (+=) ao invés de atribuir(=)
-            return self.number_served # Adicionado o retorno
+            self.number_served += more_customers  # Incrementando (+=) ao invés de atribuir(=)
+            return self.number_served  # Adicionado o return
         else:
-            return f"{self.restaurant_name} está fechado!"# retunr no lugar de print
+            return f"{self.restaurant_name} está fechado!"  # return no lugar de print
